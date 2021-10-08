fb57e9e41fdfb34cb1b082f4ee4f296195b40c13
Anusha Busetty <anusha.busetty@hexagonsi.com>
2021-10-08 19:00:31 +0530

    Implemented microsoft sign on

--- src/App.js
+++ src/App.js
@@ -1,15 +1,28 @@
-import './App.css';
-import {VerifyCertificate} from "./components/VerifyCertificate";
-import {Provider} from "react-redux";
-import {store} from "./redux/store";
+import "./App.css";
+import React from "react";
+import { VerifyCertificate } from "./components/VerifyCertificate";
+import { Provider } from "react-redux";
+import { store } from "./redux/store";
+import {
+  AuthenticatedTemplate,
+  UnauthenticatedTemplate,
+} from "@azure/msal-react";
+import { PageLayout } from "./components/PageLayout";
 
 function App() {
   return (
-      <Provider store={store}>
-        <div className="App">
+    <Provider store={store}>
+      <div className="App">
+        <PageLayout>
+          <AuthenticatedTemplate>
             <VerifyCertificate />
-        </div>
-      </Provider>
+          </AuthenticatedTemplate>
+          <UnauthenticatedTemplate>
+            <p>You are not signed in! Please sign in.</p>
+          </UnauthenticatedTemplate>
+        </PageLayout>
+      </div>
+    </Provider>
   );
 }
 
---
+++ src/components/PageLayout/index.js
@@ -0,0 +1,24 @@
+import React from "react";
+import Navbar from "react-bootstrap/Navbar";
+import { useIsAuthenticated } from "@azure/msal-react";
+import { SignInButton } from "./SignInButton";
+import { SignOutButton } from "./SignOutButton";
+
+/**
+ * Renders the navbar component with a sign-in button if a user is not authenticated
+ */
+export const PageLayout = (props) => {
+    const isAuthenticated = useIsAuthenticated();
+
+    return (
+        <>
+            <Navbar bg="secondary" variant="dark">
+                <a className="navbar-brand" href="/">Covid Certificate Verification</a>
+                {  isAuthenticated ? <SignOutButton />:<SignInButton /> }
+            </Navbar>            
+            <br />
+            <br />
+            {props.children}
+        </>
+    );
+};
